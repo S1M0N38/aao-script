16c64ae239634c520fc3054e365c6eb946f24cc6
S1M0N38 <bertolottosimone@gmail.com>
2019-02-15 15:37:08 +0100

    DB is now a class (ContexManager)

--- database.py
+++ database.py
@@ -4,9 +4,7 @@ import os
 import psycopg2
 
 
-def save_in_json(events, odds, bookmaker, path):
-    assert events
-    assert odds
+def save_events_odds_in_json(events, odds, bookmaker, path):
     country, league = events[0]['country'], events[0]['league']
     events_file = f'events_{bookmaker}_{country}_{league}.json'
     odds_file = f'odds_{bookmaker}_{country}_{league}.json'
@@ -16,67 +14,79 @@ def save_in_json(events, odds, bookmaker, path):
         json.dump(odds, f, indent=2, default=str)
 
 
-def save_in_db(events, odds, bookmaker):
-    DB_USER = os.environ['DB_USER']
-    DB_PASSWORD = os.environ['DB_PASSWORD']
-    DB_HOST = os.environ['DB_HOST']
-    DB_PORT = os.environ['DB_PORT']
+def save_events_odds_in_db(events, odds, bookmaker):
+    with DB() as db:
+        for event, odd in zip(events, odds):
+            event_id = db.search_event(event)
+            if event_id is None:
+                event_id = db.insert_event(event)
+            db.insert_odd(event_id, bookmaker, odd)
+        db.connection.commit()
 
-    def search_event(event):
-        cur.execute('''SELECT event_id FROM events WHERE (
-                    DATE(time) = %s AND
-                    country LIKE %s AND
-                    league LIKE %s AND (
-                    home_team %% %s OR
-                    away_team %% %s))
-                    LIMIT 1''',
-                    (event['datetime'].date(), event['country'],
-                     event['league'], event['home_team'], event['away_team']))
-        event_id = cur.fetchone()
-        # print(event_id)
+
+def update_events_in_db(events):
+    with DB() as db:
+        for event in events:
+            db.update_event(event)
+        db.connection.commit()
+
+
+class DB:
+
+    def __init__(self):
+        self.dbname = 'aao'
+        self.user = os.environ['DB_USER']
+        self.password = os.environ['DB_PASSWORD']
+        self.host = os.environ['DB_HOST']
+        self.port = int(os.environ['DB_PORT'])
+
+    def __enter__(self):
+        self.connection = psycopg2.connect(
+            dbname=self.dbname, user=self.user,
+            password=self.password, host=self.host, port=self.port)
+        self.cursor = self.connection.cursor()
+        return self
+
+    def __exit__(self, *args):
+        self.cursor.close()
+        self.connection.close()
+
+    def search_event(self, event):
+        self.cursor.execute(
+            ''' SELECT id FROM api_events WHERE (
+            DATE(datetime) = %s AND country = %s AND league = %s AND (
+            home_team = %s OR away_team = %s)) LIMIT 1''',
+            (event['datetime'].date(), event['country'],
+             event['league'], event['home_team'], event['away_team']))
+        event_id = self.cursor.fetchone()
         if event_id is None:
             return None
         return event_id[0]
 
-    def insert_event(event):
-        cur.execute('''INSERT INTO events
-                    (time, country, league, home_team, away_team)
-                    VALUES (%s, %s, %s, %s, %s)''',
-                    (event['datetime'], event['country'], event['league'],
-                     event['home_team'], event['away_team']))
-        cur.execute('SELECT MAX(event_id) FROM events')
-        event_id = cur.fetchone()[0]
+    def search_odd(self, event_id, bookmaker, odd):
+        ...
+
+    def insert_event(self, event):
+        self.cursor.execute(
+            '''INSERT INTO api_events (datetime, country, league, home_team,
+            away_team) VALUES (%s, %s, %s, %s, %s)''',
+            (event['datetime'], event['country'], event['league'],
+             event['home_team'], event['away_team']))
+        self.cursor.execute('SELECT MAX(id) FROM api_events')
+        event_id = self.cursor.fetchone()[0]
         return event_id
 
-    def insert_odd(event_id, bookmaker, odd):
-        assert bookmaker in ('bet365', '888sport', 'bwin', 'williamhill')
+    def insert_odd(self, event_id, bookmaker, odd):
+        self.cursor.execute(
+            '''INSERT INTO api_all_odds (datetime, bookmaker,
+            full_time_result, draw_no_bet, both_teams_to_score,
+            double_chance, under_over, event_id)
+            VALUES (current_timestamp, %s, %s, %s, %s, %s, %s, %s)''',
+            (bookmaker, json.dumps(odd['full_time_result']),
+             json.dumps(odd['draw_no_bet']),
+             json.dumps(odd['both_teams_to_score']),
+             json.dumps(odd['double_chance']), json.dumps(odd['under_over']),
+             event_id))
 
-        def dict2json(key):
-            if key in odd:
-                return json.dumps(odd[key])
-            return None
-        full_time_result = dict2json('full_time_result')
-        draw_no_bet = dict2json('draw_no_bet')
-        both_teams_to_score = dict2json('both_teams_to_score')
-        double_chance = dict2json('double_chance')
-        under_over = dict2json('under_over')
-        cur.execute('''INSERT INTO odds
-                    (event_id, time, bookmaker, full_time_result, draw_no_bet,
-                     both_teams_to_score, double_chance, under_over)
-                    VALUES (%s, current_timestamp, %s, %s, %s, %s, %s, %s)''',
-                    (event_id, bookmaker, full_time_result, draw_no_bet,
-                     both_teams_to_score, double_chance, under_over))
-
-    conn = psycopg2.connect(
-        dbname='aao', user=DB_USER, password=DB_PASSWORD,
-        host=DB_HOST, port=int(DB_PORT)
-    )
-    cur = conn.cursor()
-    for event, odd in zip(events, odds):
-        event_id = search_event(event)
-        if event_id is None:
-            event_id = insert_event(event)
-        insert_odd(event_id, bookmaker, odd)
-    conn.commit()
-    cur.close()
-    conn.close()
+    def update_event(self, event):
+        ...
